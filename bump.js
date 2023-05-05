a85e267663377776193a0617d17b3fc97bdd6359
Chris <chrisbreuer93@gmail.com>
2023-05-05 10:43:50 -0700

    chore: initial commit

---
+++ bump.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const path = require('path');
+
+const packagePath = path.join(__dirname, 'package.json');
+const { version } = JSON.parse(fs.readFileSync(packagePath, {
+    encoding: 'utf8'
+}));
+
+console.log("package.json: ", version);
+
+const composerPath = path.join(__dirname, 'composer.json');
+const composer = JSON.parse(fs.readFileSync(composerPath, {
+    encoding: 'utf8'
+}));
+
+composer.version = version;
+console.log("composer.json: ", version);
+
+fs.writeFileSync(composerPath, JSON.stringify(composer, null, 2));
+
+const packagesDir = path.join(__dirname, 'packages');
+const packageDirs = fs.readdirSync(packagesDir, { withFileTypes: true })
+    .filter(dirent => dirent.isDirectory())
+    .map(dirent => dirent.name);
+
+const packages = packageDirs.map(async packageDir => {
+    const packageComposerPath = path.join(packagesDir, packageDir, 'composer.json')
+
+    const packageComposer = JSON.parse(fs.readFileSync(packageComposerPath, {
+        encoding: 'utf8'
+    }));
+
+    packageComposer.version = version;
+    console.log("package: " + packageDir, "version: " + packageComposer.version);
+    fs.writeFileSync(packageComposerPath, JSON.stringify(packageComposer, null, 2));
+});
+
